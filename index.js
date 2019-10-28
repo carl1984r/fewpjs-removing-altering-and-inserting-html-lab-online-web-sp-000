2a99a48de47e59a6a8a4759c6d87b4b957a460cc
Bry Cox <carl.cc@gmail.com>
2019-10-28 14:58:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ main.remove();
 let newHeader = document.createElement('h1');
 document.body.appendChild(newHeader);
 newHeader.setAttribute("id", "victory")
+newHeader.innerHTML = "YOUR-NAME is the champion"
